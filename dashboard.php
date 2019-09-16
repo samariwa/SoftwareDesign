faf3b31fdce5c08a2eb0abfbd230c4b48dfcac46
Mariwa <samuelmariwa@gmail.com>
2019-09-15 20:34:26 -0400

    This is the dashboard styling. Add the folder assets to root folder and copy whats there and make changes to dashboard

---
+++ dashboard.php
@@ -0,0 +1,16 @@
+<ul class="cb-slideshow">
+            <li><span></span><div><h3>Co·nv·e·nien·ce</h3></div></li>
+            <li><span></span><div><h3 style="color:black;">Fa·st</h3></div></li>
+            <li><span></span><div><h3>E·asy</h3></div></li>
+            <li><span></span><div><h3 style="color:black;">A·pp·ly</h3></div></li>
+            <li><span></span><div><h3 style="color:black;">Ex·pe·rienc·e</h3></div></li>
+            <li><span></span><div><h3>Va·r·ie·ty</h3></div></li>
+</ul>
+<div class="container">
+    <!-- Codrops top bar -->
+    <div class="codrops-top">
+        
+        
+        <div class="clr"></div>
+    </div><!--/ Codrops top bar -->
+</div>
