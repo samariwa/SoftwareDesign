dda735b0ee04cec95632b9e95a84a5e5a86dd50f
Mariwa <samuelmariwa@gmail.com>
2019-09-19 13:42:26 -0400

    the additional images and a start on the admin interface

---
+++ pages/adminSelect.php
@@ -0,0 +1,32 @@
+<ul class="cb-slideshowAdmin">
+            <li><span></span><div></div></li>
+            <li><span></span><div></div></li>
+            <li><span></span><div></div></li>
+            <li><span></span><div></div></li>
+            <li><span></span><div></div></li>
+            <li><span></span><div></div></li>
+            <br><br><br><br><br><br><br><br><br><br><br>
+     <div class="form-group">
+    <select class="custom-select" required style="border-radius: 20px;" onchange="location = this.options[this.selectedIndex].value;" >
+      <option value="" selected>Select Action to Perform</option>
+      <option value="http://192.168.64.2/lec1/CiProject.10/newJobForm">Create New Job</option>
+      <option value="http://192.168.64.2/lec1/CiProject.10/adminJobList">View List of Jobs With The Number of Slots</option>
+      <option value="3">View New Applications</option>
+      <option value="4">View Accepted Applications</option>
+      <option value="5">View Rejected Applications</option>
+      <option value="6">View Pending Interviews</option>
+    </select>
+  </div>
+  <br><br><br><br><br><br><br><br><br><br><br><br>
+</ul>
+<br>
+<div class="container">
+    <!-- Codrops top bar -->
+
+    <div class="codrops-top">
+        
+        
+        <div class="clr"></div>
+    </div><!--/ Codrops top bar -->
+</div>
+ 
---
+++ pages/newJobForm.php
@@ -0,0 +1,22 @@
+<br/><br/>
+<h1>Create New Job</h1>
+        <hr/>
+        <form action=""method="POST">
+            <div class="form-group">
+                <label name="title">Job Title:</label>
+                <input id="title" name="title" class="form-control">
+            </div>
+            <div class="form-group">
+                <label name="description">Job Description:</label>
+                <textarea id="description" name="description" class="form-control">Enter Full Job Description Here...</textarea>
+            </div>
+            <div class="form-group">
+                <label name="slots">Number of Slots Available:</label>
+                <input id="slots" name="slots" class="form-control">
+            </div>
+            <div class="form-group">
+                <label name="requirements">Job Requirements:</label>
+                <textarea id="requirements" name="requirements" class="form-control">Enter the Requirements Here... </textarea>
+            </div>
+            <input type="submit" value="Add Job" class="btn btn-success btn-block">
+        </form>    
